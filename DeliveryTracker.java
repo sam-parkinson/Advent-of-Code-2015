f4825217b8ae08bda463c345ea829464c6702a94
Sam Parkinson <parkinsonsp42@gmail.com>
2022-03-22 13:39:17 -0400

    Update day 3

--- DeliveryTracker.java
+++ DeliveryTracker.java
@@ -10,9 +10,47 @@ public class DeliveryTracker {
         directions = makeDirections(address);
     }
 
+    public int getTotalHouses() {
+        return coordinates.size();
+    }
+
     private char[] makeDirections(String address) {
+        String input = "";
 
+        try {
+            File file = new File(address);
+            Scanner stdin = new Scanner(file);
 
-        return null;
+            input = stdin.nextLine();
+
+            stdin.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        char[] directions = new char[input.length()];
+
+        for (int i = 0; i < directions.length; i++) {
+            directions[i] = input.charAt(i);
+        }
+
+        return directions;
+    }
+
+    private void followDirections() {
+        // {x, y}
+        // > right, < left
+        // 
+        Integer[] coords = {0, 0};
+
+        coordinates.put(coords, 1);
+
+        // start at 0, 0
+
+        // go through directions
+        // move
+        // check to see if coords exist
+        // if not, add
+        // if so, increment
     }
 }
--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ public class Driver {
     public static void main(String[] args) {
         FakeLisp fakeLisp = new FakeLisp("inputs/day1.txt");
         PaperCalculator paperCalc = new PaperCalculator("inputs/day2.txt");
+        DeliveryTracker tracker = new DeliveryTracker("inputs/day3.txt");
 
         System.out.println("Problem 1.1: " + fakeLisp.getFloor());
         System.out.println("Problem 1.2: " + fakeLisp.getBasement());
@@ -9,5 +10,8 @@ public class Driver {
         System.out.println();
         System.out.println("Problem 2.1: " + paperCalc.getFootage());
         System.out.println("Problem 2.2: " + paperCalc.getLength());
+
+        System.out.println();
+        System.out.println("Problem 3.1: " + tracker.getTotalHouses());
     }
 }
