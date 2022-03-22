b6c1cfcb6731412f34fbce4b2b4cc7765b248595
Sam Parkinson <parkinsonsp42@gmail.com>
2022-03-22 12:23:56 -0400

    Begin day 3

---
+++ DeliveryTracker.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DeliveryTracker {
+    private char[] directions;
+    private HashMap<Integer[], Integer> coordinates;
+
+    public DeliveryTracker(String address) {
+        directions = makeDirections(address);
+    }
+
+    private char[] makeDirections(String address) {
+
+
+        return null;
+    }
+}
