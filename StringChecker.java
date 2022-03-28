569185df176381df05658e1410dc7c2fcf82e5bd
Sam Parkinson <43306113+sam-parkinson@users.noreply.github.com>
2022-03-28 02:09:59 -0400

    Begin day 5

---
+++ StringChecker.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StringChecker {
+    private char[][] splitStrings;
+    private int niceCount;
+
+    public StringChecker(String address) {
+
+    }
+
+    
+}
